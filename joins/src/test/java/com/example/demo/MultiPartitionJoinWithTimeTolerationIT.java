c43e97e85d6a3f509ccc388f4665851868c6381a
mbimbij <joseph.mbimbi@gmail.com>
2021-01-31 22:16:45 +0100

    Improve the testing of the join window toleration using kafka headers and specifying the timestamp instead of waiting

--- joins/src/test/java/com/example/demo/MultiPartitionJoinWithTimeTolerationIT.java
+++ joins/src/test/java/com/example/demo/MultiPartitionJoinWithTimeTolerationIT.java
@@ -11,36 +11,32 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 import java.util.Properties;
 
 import static com.example.demo.MultiPartitionJoinWithTimeToleration.*;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+import static org.springframework.kafka.support.KafkaHeaders.TOPIC;
 
-@SpringBootTest
-@ContextConfiguration(
-    classes = {ConfigurationIT.class}
-    , initializers = MultiPartitionJoinWithTimeTolerationIT.class
-)
-@EmbeddedKafka(
-    topics = {INPUT_TOPIC_A, INPUT_TOPIC_B, OUTPUT_TOPIC}
-)
+@SpringBootTest(properties = "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}")
+@ContextConfiguration(classes = ConfigurationIT.class)
+@EmbeddedKafka(topics = {INPUT_TOPIC_A, INPUT_TOPIC_B, OUTPUT_TOPIC})
 @ActiveProfiles("test")
 @TestInstance(PER_CLASS)
 @Slf4j
-class MultiPartitionJoinWithTimeTolerationIT implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+class MultiPartitionJoinWithTimeTolerationIT {
   @Autowired
   private KafkaTemplate<String, String> producer;
   @Autowired
@@ -48,19 +44,11 @@ class MultiPartitionJoinWithTimeTolerationIT implements ApplicationContextInitia
   @Autowired
   private TestListenerIT listener;
   private KafkaStreams kafkaStreams;
-  private final int tolerationWindowSeconds = 3;
-
-  @Override
-  public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-    String embeddedKafkaAddress = configurableApplicationContext.getEnvironment().getProperty("spring.embedded.kafka.brokers");
-    TestPropertyValues.of(
-        String.format("spring.kafka.bootstrap-servers[0]=%s", embeddedKafkaAddress)
-    ).applyTo(configurableApplicationContext.getEnvironment());
-  }
+  private static final int TOLERATION_WINDOW_SECONDS = 3;
 
   @BeforeAll
   void beforeAll() {
-    MultiPartitionJoinWithTimeToleration streamApplication = new MultiPartitionJoinWithTimeToleration(embeddedKafkaBroker.getBrokersAsString(), tolerationWindowSeconds);
+    MultiPartitionJoinWithTimeToleration streamApplication = new MultiPartitionJoinWithTimeToleration(embeddedKafkaBroker.getBrokersAsString(), TOLERATION_WINDOW_SECONDS);
     Topology topology = streamApplication.getTopology();
     Properties properties = streamApplication.getProperties();
     kafkaStreams = new KafkaStreams(topology, properties);
@@ -83,22 +71,37 @@ class MultiPartitionJoinWithTimeTolerationIT implements ApplicationContextInitia
   @ParameterizedTest
   @CsvSource({
       "0,  v1-1, v2-1",
-      "2, toto, tata"
+      (TOLERATION_WINDOW_SECONDS - 1) + ", toto, tata"
   })
   public void shouldJoin_whenSecondEventArrivesWithinTheTolerationWindow(int delaySeconds, String valueFirstRecord, String valueSecondRecord)
       throws Exception {
     // given
     String expectedRecord = String.join(",", valueFirstRecord, valueSecondRecord);
+    long nowMillis = Instant.now().toEpochMilli();
     log.info("expected record: {}", expectedRecord);
 
     // when
-    producer.send(INPUT_TOPIC_A, "k1", valueFirstRecord);
-    Thread.sleep(delaySeconds * 1000L);
-    producer.send(INPUT_TOPIC_B, "k1", valueSecondRecord);
+    Message<String> message1 = MessageBuilder
+        .withPayload(valueFirstRecord)
+        .setHeader(KafkaHeaders.MESSAGE_KEY, "k1")
+        .setHeader(KafkaHeaders.TIMESTAMP, nowMillis)
+        .setHeader(TOPIC, INPUT_TOPIC_A)
+        .build();
+    Message<String> message2 = MessageBuilder
+        .withPayload(valueSecondRecord)
+        .setHeader(KafkaHeaders.MESSAGE_KEY, "k1")
+        .setHeader(KafkaHeaders.TIMESTAMP, nowMillis + delaySeconds * 1000L)
+        .setHeader(TOPIC, INPUT_TOPIC_B)
+        .build();
+    producer.send(message1);
+    producer.send(message2);
+//    producer.send(INPUT_TOPIC_A, "k1", valueFirstRecord);
+//    Thread.sleep(delaySeconds * 1000L);
+//    producer.send(INPUT_TOPIC_B, "k1", valueSecondRecord);
 
     // then
     List<String> joinedRecords = await()
-        .atMost(Duration.ofSeconds(5))
+        .atMost(Duration.ofSeconds(3))
         .until(() -> listener.getByKey("k1"), list -> list.contains(expectedRecord));
   }
 }
