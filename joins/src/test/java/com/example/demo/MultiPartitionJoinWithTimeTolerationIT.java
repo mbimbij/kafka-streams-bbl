5a238b8624f90c833239a3403aa8cd5500c92acc
mbimbij <joseph.mbimbi@gmail.com>
2021-01-31 22:39:57 +0100

    Add an integration test for a join with event outside of the toleration window

--- joins/src/test/java/com/example/demo/MultiPartitionJoinWithTimeTolerationIT.java
+++ joins/src/test/java/com/example/demo/MultiPartitionJoinWithTimeTolerationIT.java
@@ -3,10 +3,8 @@ package com.example.demo;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.Topology;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.TestInstance;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +24,7 @@ import java.util.List;
 import java.util.Properties;
 
 import static com.example.demo.MultiPartitionJoinWithTimeToleration.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import static org.springframework.kafka.support.KafkaHeaders.TOPIC;
@@ -73,14 +72,11 @@ class MultiPartitionJoinWithTimeTolerationIT {
       "0,  v1-1, v2-1",
       (TOLERATION_WINDOW_SECONDS - 1) + ", toto, tata"
   })
-  public void shouldJoin_whenSecondEventArrivesWithinTheTolerationWindow(int delaySeconds, String valueFirstRecord, String valueSecondRecord)
-      throws Exception {
+  public void shouldJoin_whenSecondEventArrivesWithinTheTolerationWindow(int delaySeconds, String valueFirstRecord, String valueSecondRecord) {
     // given
     String expectedRecord = String.join(",", valueFirstRecord, valueSecondRecord);
     long nowMillis = Instant.now().toEpochMilli();
     log.info("expected record: {}", expectedRecord);
-
-    // when
     Message<String> message1 = MessageBuilder
         .withPayload(valueFirstRecord)
         .setHeader(KafkaHeaders.MESSAGE_KEY, "k1")
@@ -93,6 +89,8 @@ class MultiPartitionJoinWithTimeTolerationIT {
         .setHeader(KafkaHeaders.TIMESTAMP, nowMillis + delaySeconds * 1000L)
         .setHeader(TOPIC, INPUT_TOPIC_B)
         .build();
+
+    // when
     producer.send(message1);
     producer.send(message2);
 //    producer.send(INPUT_TOPIC_A, "k1", valueFirstRecord);
@@ -103,5 +101,35 @@ class MultiPartitionJoinWithTimeTolerationIT {
     List<String> joinedRecords = await()
         .atMost(Duration.ofSeconds(3))
         .until(() -> listener.getByKey("k1"), list -> list.contains(expectedRecord));
+  }
+
+  @Test
+  public void shouldNotJoin_whenSecondEventArrivesAfterTheTolerationWindow() {
+    // given
+    String expectedRecord = String.join(",", "v1-1", "v1-2");
+    long nowMillis = Instant.now().toEpochMilli();
+    log.info("expected record: {}", expectedRecord);
+    long delaySeconds = TOLERATION_WINDOW_SECONDS + 1;
+    Message<String> message1 = MessageBuilder
+        .withPayload("v1-1")
+        .setHeader(KafkaHeaders.MESSAGE_KEY, "k1")
+        .setHeader(KafkaHeaders.TIMESTAMP, nowMillis)
+        .setHeader(TOPIC, INPUT_TOPIC_A)
+        .build();
+    Message<String> message2 = MessageBuilder
+        .withPayload("v1-2")
+        .setHeader(KafkaHeaders.MESSAGE_KEY, "k1")
+        .setHeader(KafkaHeaders.TIMESTAMP, nowMillis + delaySeconds * 1000L)
+        .setHeader(TOPIC, INPUT_TOPIC_B)
+        .build();
+
+    // when
+    producer.send(message1);
+    producer.send(message2);
+
+    // then - waiting for the join result throws an exception - since it is outside of the toleration window
+    assertThatThrownBy(() -> await()
+        .atMost(Duration.ofSeconds(3))
+        .until(() -> listener.getByKey("k1"), list -> list.contains(expectedRecord)));
   }
 }
